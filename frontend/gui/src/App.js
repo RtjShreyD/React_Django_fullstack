557d9747f53a6c232b6a38b5c6e1b490ff0af2dd
RtjShreyD <shreyanshece1041@gmail.com>
2020-01-18 15:57:26 +0530

    ArticleListView added

--- frontend/gui/src/App.js
+++ frontend/gui/src/App.js
@@ -3,13 +3,14 @@ import './App.css';
 import 'antd/dist/antd.css';
 
 import CustomLayout from './containers/Layout';
+import ArticleList from './containers/ArticleListView';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <CustomLayout>
-
+          <ArticleList />
         </CustomLayout>
       </div>
     );
