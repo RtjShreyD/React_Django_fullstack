783557e9f0abe0350cfca0aced15f2513e985156
RtjShreyD <shreyanshece1041@gmail.com>
2020-01-18 15:38:01 +0530

    App.js now using OOPs approach, ELIFECYCLE solved

--- frontend/gui/src/App.js
+++ frontend/gui/src/App.js
@@ -1,17 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import 'antd/dist/antd.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1 className="App-title">Welcome to React</h1>
-      </header>
-      <p className="App-intro">
-        To get started, edit <code>src/App.js</code> and save to reload
-      </p>
-    </div>
-  );
+import CustomLayout from './containers/Layout';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <CustomLayout>
+
+        </CustomLayout>
+      </div>
+    );
+  }
 }
 
 export default App;
